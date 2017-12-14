b213fbb5baf5d8419f5771177842f63374839054
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-12-13 22:00:24 -0500

    Added ContentViewCell

--- MAPD714-F2017-Lesson11B/ViewController.swift
+++ MAPD714-F2017-Lesson11B/ViewController.swift
@@ -1,25 +1,10 @@
-//
-//  ViewController.swift
-//  MAPD714-F2017-Lesson11B
-//
-//  Created by Tom Tsiliopoulos on 2017-12-13.
-//  Copyright © 2017 Centennial College. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
