1df472b74d08059ebb1504d717a7fb38f7c826b6
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-12-13 22:41:03 -0500

    Added support for spacing and Header (with bugs)

--- MAPD714-F2017-Lesson11B/ViewController.swift
+++ MAPD714-F2017-Lesson11B/ViewController.swift
@@ -1,10 +1,83 @@
 import UIKit
 
-class ViewController: UICollectionViewController {
+class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
+    var sections = [
+        ["header":"First Witch", "content":"When will we meet up later?"],
+        ["header":"Second Witch", "content":"When everything's ok"],
+        ["header":"Third Witch", "content":"before sunset"],
+        ["header":"First Witch", "content":"Where?"],
+        ["header":"Second Witch", "content":"The Circle"],
+        ["header":"Third Witch", "content":"I'll see you there"],
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+        self.collectionView?.register(ContentCell.self, forCellWithReuseIdentifier: "CONTENT")
+        var contentInset = collectionView!.contentInset
+        contentInset.top = 20
+        collectionView!.contentInset = contentInset
+        
+        let layout = collectionView!.collectionViewLayout
+        let flow = layout as! UICollectionViewFlowLayout
+        flow.sectionInset = UIEdgeInsetsMake(10, 20, 30, 20)
+        
+        self.collectionView?.register(HeaderCell.self, forCellWithReuseIdentifier: "HEADER")
+        
+        flow.headerReferenceSize = CGSize(width: 100, height: 25)
     }
+    
+    func wordsInSection(section: Int) -> [String] {
+        let content = sections[section]["content"]
+        let spaces = NSCharacterSet.whitespacesAndNewlines
+        let words = content?.components(separatedBy: spaces)
+        return words!
+    }
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return sections.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        let words = wordsInSection(section: section)
+        return words.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let words = wordsInSection(section: indexPath.section)
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CONTENT", for: indexPath) as! ContentCell
+        cell.maxWidth = collectionView.bounds.size.width
+        cell.text = words[indexPath.row]
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        let words = wordsInSection(section: indexPath.section)
+        let size = ContentCell.sizeForContentString(s: words[indexPath.row], forMaxWidth: collectionView.bounds.size.width)
+        return size
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        if(kind == UICollectionElementKindSectionHeader) {
+            let cell = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "HEADER", for: indexPath) as! HeaderCell
+            cell.maxWidth = collectionView.bounds.size.width
+            cell.text = sections[indexPath.section]["header"]
+            return cell
+        }
+        abort()
+    }
+    
+    
 }
+
+
+
+
+
+
+
+
+
 
